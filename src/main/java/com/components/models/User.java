db94946eee843ef1b09fbbb588fccbfdde447cce
ruslan <yarmet84@mail.ru>
2017-09-17 06:32:31 +0300

    Cделал поле username unique, Добавил метод для получения текущего времени в формате UTC-0

--- src/main/java/com/components/models/User.java
+++ src/main/java/com/components/models/User.java
@@ -18,7 +18,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @Column(name = "username")
+    @Column(name = "username", unique = true)
     private String username;
 
     @Column(name = "password")
--- src/main/java/com/components/utils/Utils.java
+++ src/main/java/com/components/utils/Utils.java
@@ -2,6 +2,10 @@ package com.components.utils;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.sql.Timestamp;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Collection;
 
 public class Utils {
@@ -9,5 +13,11 @@ public class Utils {
     public static Collection<GrantedAuthority> getUserRoles() {
         return (Collection<GrantedAuthority>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
     }
+
+
+    public static Timestamp getCurrentTimestampAsUTC() {
+        return Timestamp.valueOf(ZonedDateTime.now(ZoneOffset.UTC).toLocalDateTime());
+    }
+
 
 }
